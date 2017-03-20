72a6ced2776902c68c5747782a5fc4947584701d
twister077 <twister077@users.noreply.github.com>
2017-03-20 13:11:45 +0100

    Update pyramid_example.py

--- Examples/Model-View-Controller/pyramid_example.py
+++ Examples/Model-View-Controller/pyramid_example.py
@@ -7,7 +7,7 @@ from pyramid.static import static_view
 if __name__=='__main__':
     config = Configurator()
     config.add_route('index', '/')
-    config.scan(package='views')
+    config.scan(package='controller')
     config.add_static_view(name='static', path='static')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
